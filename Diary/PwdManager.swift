7333d4b0896b0a47bf202d7e4b1365559296f304
KO <kj.kozk@gmail.com>
2018-04-01 11:37:00 +0900

    パスワード要求、途中

---
+++ Diary/PwdManager.swift
@@ -0,0 +1,9 @@
+//
+//  PwdManager.swift
+//  Diary
+//
+//  Created by KO on 2018/03/29.
+//  Copyright © 2018年 KO. All rights reserved.
+//
+
+import Foundation
---
+++ Diary/PwdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PwdViewController.swift
+//  Diary
+//
+//  Created by KO on 2018/03/29.
+//  Copyright © 2018年 KO. All rights reserved.
+//
+
+import UIKit
+
+class PwdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
